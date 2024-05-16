ee7ebb9079a3e6dac2b3e4ceaca05d527e063c69
Muruts Gebretsadik <murutsmenge21@Murutss-MacBook-Air.local>
2024-05-16 14:09:55 +0100

    update views.py

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserRegisterForm
-from django.contrib import messages #import for messages
+from django.contrib.auth.decorators import login_required #Added import here
+from django.contrib import messages
+from .forms import UserRegisterForm
 
 # Create your views here.
 def register(request):
@@ -9,8 +10,12 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Account created successfully, now you can login.')
+            return redirect('login') 
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+@login_required # Added decorator here
+def profile(request): 
+    return render(request, 'users/profile.html')
