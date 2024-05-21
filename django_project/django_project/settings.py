012795aeb85541ab2f1804790ea42488dc2db8fd
Muruts Gebretsadik <murutsmenge21@Murutss-MacBook-Air.local>
2024-05-21 19:12:29 +0100

    return update seeting

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -10,19 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-from dotenv import load_dotenv
-load_dotenv()
-
-import os
-import dj_database_url
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-#keep secret key
-SECRET_KEY = str(os.environ.get('SECRET_KEY'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -54,7 +45,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -87,20 +77,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# Replace the SQLite DATABASES configuration with PostgreSQL:
-DATABASE_URL = os.environ.get('DATABASE_URL')
-
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 
 
 # Password validation
@@ -139,14 +121,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# This production code might break development mode, so we check whether we're in DEBUG mode
-if not DEBUG:
-    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-    # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
@@ -165,8 +139,4 @@ LOGIN_URL = 'login'
 # to store uploaded files
 MEDIA_ROOT = BASE_DIR / 'media' # Newer modern and Pythonic way 
 MEDIA_URL = '/media/'
-
-
-
-
 
