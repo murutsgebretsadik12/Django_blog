b2e44bc16761cd571d9e1168a75673b47d04a13c
Muruts Gebretsadik <murutsmenge21@Murutss-MacBook-Air.local>
2024-05-21 18:20:44 +0100

    create .env

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -21,6 +21,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#keep secret key
+SECRET_KEY = str(os.environ.get('SECRET_KEY'))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
