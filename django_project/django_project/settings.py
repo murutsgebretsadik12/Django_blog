a48543a8c2c269b45d203f9986e41ae3302653d7
Muruts Gebretsadik <murutsmenge21@Murutss-MacBook-Air.local>
2024-05-25 11:42:46 +0100

    update the settings.py

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from dotenv import load_dotenv
 load_dotenv()
 
+import dj_database_url
+
 import os, certifi
 
 os.environ['SSL_CERT_FILE'] = certifi.where()
@@ -26,16 +28,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
 
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY  = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 
-
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
 
 # Application definition
 
@@ -87,11 +88,15 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
