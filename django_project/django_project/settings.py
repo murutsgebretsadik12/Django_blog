43fe68456cf4b8e9da1d70a112c5e09aedd055e4
Muruts Gebretsadik <murutsmenge21@Murutss-MacBook-Air.local>
2024-05-25 10:11:12 +0100

    update the setting and .env

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -27,12 +27,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'abcdxyz132'
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
+
+SECRET_KEY  = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
 
 
 # Application definition
@@ -52,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
